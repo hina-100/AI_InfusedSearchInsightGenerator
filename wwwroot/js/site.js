89d0b7b09f459af5d071177b86a95cc0c89058dd
Hina Agrawal <hina.agrawal@optym.com>
2024-04-06 17:50:08 +0530

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+Share = {
+    facebook: function (purl, ptitle, pimg, text) {
+        url = 'http://www.facebook.com/sharer.php?s=100';
+        url += '&p[title]=' + encodeURIComponent(ptitle);
+        url += '&p[summary]=' + encodeURIComponent(text);
+        url += '&p[url]=' + encodeURIComponent(purl);
+        url += '&p[images][0]=' + encodeURIComponent(pimg);
+        Share.popup(url);
+    },
+    twitter: function (purl, ptitle) {
+        url = 'http://twitter.com/share?';
+        url += 'text=' + encodeURIComponent(ptitle);
+        url += '&url=' + encodeURIComponent(purl);
+        url += '&counturl=' + encodeURIComponent(purl);
+        Share.popup(url);
+    },
+    popup: function (url) {
+        window.open(url, '', 'toolbar=0,status=0,width=626, height=436');
+    }
+};
